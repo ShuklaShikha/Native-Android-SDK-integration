1b544e63b9dd3cf33197bd91339d9429f531effd
Shikha ShuklaCT <shikha@clevertap.com>
2023-03-11 22:56:29 +0530

    init commit

---
+++ app/src/main/java/com/example/clevertaptestapp/onuserlogin2.java
@@ -0,0 +1,14 @@
+package com.example.clevertaptestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class onuserlogin2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onuserlogin2);
+    }
+}
